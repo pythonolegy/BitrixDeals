9f026799bb9b7616a9ddd1fca328eada6bf94c87
pythonolegy <olegkachalin20@gmail.com>
2022-05-26 21:17:51 +0200

    -final

--- app/server.py
+++ app/server.py
@@ -5,20 +5,11 @@ from main import *
 
 @post('/update')
 def post_deal():
-    # print(bt_find_by_delivery_code()[0]['ID'])
-    # print(get_info_by_id()['UF_CRM_DELIVERY_PRODUCTS'])
-    # add_deal(request.json,'products')
-    print(valid_delivery_fields())
-   #  print(add_products(request.json, 'products'))
-# @post('/update')
-# def post_deal():
-#     try:
-#         if not valid_contact():
-#             return 'contact is already exist'
-#         valid_contact()
-#         return 'contact has been added'
-#     except KeyError:
-#         return 'Invalid keys'
-
-
+    # try:
+    #     print(valid_contact())
+    #     "Операция совершена успешно"
+    # except KeyError:
+    #     return "Проверьте правильность введеных данных"
+    # # print(update_contact())
+    print(valid_delivery_code())
 run(host='localhost', port=8080)
