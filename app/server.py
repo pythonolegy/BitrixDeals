a525de1ea4d4356a49324d6226f1f5dc71c932c4
pythonolegy <olegkachalin20@gmail.com>
2022-05-27 03:16:07 +0200

    -final

--- app/server.py
+++ app/server.py
@@ -1,15 +1,15 @@
 from bottle import run, post
 
-from main import main
+from main import *
 
 
 @post('/update')
 def post_deal():
-    try:
-        return main()
-    except KeyError:
-        return 'Проверьте введенные данные'
-
+    # try:
+    #     return main()
+    # except KeyError:
+    #     return 'Проверьте введенные данные'
+    print(get_info_by_id())
 
 run(host='localhost', port=8080)
 
